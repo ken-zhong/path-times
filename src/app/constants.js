970cd3980f3a071d6d508a9f5ff11fff171977d4
Kennan Zhong <kenzhong@gmail.com>
2019-11-22 16:53:12 +0800

    init react app

---
+++ src/app/constants.js
@@ -0,0 +1,15 @@
+export const STATIONS = [
+  'newark',
+  'harrison',
+  'journal_square',
+  'grove_street',
+  'exchange_place',
+  'world_trade_center',
+  'newport',
+  'hoboken',
+  'christopher_street',
+  'ninth_street',
+  'fourteenth_street',
+  'twenty_third_street',
+  'thirty_third_street'
+];
